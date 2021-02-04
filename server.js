220028fe4dbbc1fe69715eefa84a8011bf21bf51
Serge Ilyashenko <serj.ilyashenko@gmail.com>
2021-02-04 17:28:04 +0300

    feat(server): meduim tutorial test;

--- server.js
+++ server.js
@@ -1,36 +1,46 @@
 const puppeteer = require('puppeteer');
 
+const getHref = (page, selector) =>
+  page.evaluate(
+    selector => document.querySelector(selector).getAttribute('href'),
+    selector
+  );
+
 async function startWebPage() {
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 100,    // slows down Puppeteer operations
-    devtools: false  // close dev tools
+    slowMo: 50,    // slows down Puppeteer operations
+    devtools: true  // close dev tools
   });
 
-  const page = await browser.newPage();
-  await page.setViewport({ width: 700, height: 900 });
+  try {
+    const page = await browser.newPage();
+    await page.setViewport({ width: 700, height: 900 });
 
-  const link = 'https://www.google.com';
-  await page.goto(link);
+    const link = 'https://www.google.com';
+    await page.goto(link);
 
-  await page.waitForSelector('div form div:nth-child(2) input');
-  await page.click('div form div:nth-child(2) input');
-  await page.keyboard.type(process.argv[2] || 'JavaScript');
-  await page.keyboard.press('Enter');
+    await page.waitForSelector('div form div:nth-child(2) input');
+    await page.click('div form div:nth-child(2) input');
+    await page.keyboard.type(process.argv[2] || 'JavaScript');
+    await page.keyboard.press('Enter');
 
-  await page.waitForSelector('#main > div #center_col #search > div > div > div');
+    await page.waitForSelector('#main > div #center_col #search > div > div > div');
 
-  const getHref = (page, selector) =>
-    page.evaluate(
-      selector => document.querySelector(selector).getAttribute('href'),
-      selector
-    );
-  const url = await getHref(page, `#main > div #center_col #search > div > div > div a`);
+    const url = await getHref(page, `#main > div #center_col #search > div > div > div a`);
 
-  // await page.waitFor(3000);
-  await browser.close();
+    console.log('>> ', url);
 
-  console.log('>> ', url);
+    await page.screenshot({
+      fullPage: true,
+      path: 'new_image.png'
+    });
+
+    await browser.close();
+  } catch(error) {
+    console.log(error);
+    await browser.close();
+  }
 }
 
 startWebPage();
